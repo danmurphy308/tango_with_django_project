c7ee271386a489ab1203e7d5f1a82ae6ff626c9a
Dan Murphy <3040225M@student.gla.ac.uk>
2026-02-10 21:30:53 +0000

    ch3: finalised and fixed index and about views per book

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]
-
