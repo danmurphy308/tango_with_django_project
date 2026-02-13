ea8e4a1f46993db5bfaef0e6dde5a26be7a73d43
Dan Murphy <3040225M@student.gla.ac.uk>
2026-02-13 14:33:12 +0000

    got the link add_page working

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from rango.models import Category, Page
+from rango.models import Page, Category
 
 
 class CategoryForm(forms.ModelForm):
@@ -12,3 +12,25 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ('name',)
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128,
+                            help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200,
+                         help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not (url.startswith('http://') or url.startswith('https://')):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
+
