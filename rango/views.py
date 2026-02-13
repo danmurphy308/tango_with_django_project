51fd88fdb8903030d23a1d51df18f314cbbe1736
Dan Murphy <3040225M@student.gla.ac.uk>
2026-02-13 01:26:37 +0000

    chapter 5: added categories on index page

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category   
+
 
 def index(request):
-    return render(request, 'rango/index.html')
+    category_list = Category.objects.all()
+    context_dict = {'categories': category_list}
+
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
